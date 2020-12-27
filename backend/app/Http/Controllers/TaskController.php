7be2104ada68d6674850480a0a83ba3d90b6991a
simonnaohiro <dreamtheater117@me.com>
2020-12-27 20:49:13 +0900

    feat create folderCreate

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -19,6 +19,7 @@ class TaskController extends Controller
         //選ばれたフォルダに紐づくタスクを取得する
         $tasks = $current_folder->tasks()->get();
 
+        
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-
+use App\Http\Controllers\FolderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +20,6 @@ Route::get('/', function () {
 });
 
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [FolderController::class, 'create']);
