7d4abfa4cdf776a99b03359a7534e7c632e5d4a3
simonnaohiro <dreamtheater117@me.com>
2020-12-28 18:13:54 +0900

    feat create task

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,7 +19,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// タスクのルート
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+Route::post('/folders/{id}/tasks/create' ,[TaskController::class, 'create']);
 
+// フォルダのルート
 Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
 Route::post('/folders/create', [FolderController::class, 'create']);
+
